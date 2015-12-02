b11ab8874e21d3cc819148ab887a5ef514095e88
William Dmitri Breaden Madden <william.dmitri.breaden.madden@cern.ch>
2015-12-02 15:44:41 +0100

    add setup.py, add MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "propyte",
+        version          = "2015.11.25.1340",
+        description      = "template Python program",
+        long_description = Markdown_to_reStructuredText("README.md"),
+        url              = "https://github.com/wdbm/propyte",
+        author           = "Will Breaden Madden",
+        author_email     = "w.bm@cern.ch",
+        license          = "GPLv3",
+        py_modules       = ["v"],
+        entry_points     = """
+            [console_scripts]
+            propyte = propyte:propyte
+        """
+    )
+
+def read(*paths):
+    with open(os.path.join(*paths), "r") as filename:
+        return filename.read()
+
+def Markdown_to_reStructuredText(filename):
+    try:
+        import pypandoc
+        return pypandoc.convert(filename, "rst")
+    except:
+        print("pypandoc not found; long description could be corrupted")
+        return read(filename)
+
+if __name__ == "__main__":
+    main()
